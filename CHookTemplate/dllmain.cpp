76317bc67e05b7c98dc20716259eac206716ded1
marlkiller <marlkiller@voidm.com>
2021-09-03 13:13:06 +0800

    'dev'

--- CHookTemplate/dllmain.cpp
+++ CHookTemplate/dllmain.cpp
@@ -26,6 +26,33 @@ void UnLoad(HMODULE module) {
 
 void PrintLogger(DWORD eax);
 
+//AddChatMsg
+//Module Base Address 7AF20000
+//7B493420    E8 9F619300     call WeChatWi.7BDC95C4                   ; 日志HOOK
+//LoggerReturnAddress
+
+DWORD LoggerCallAddress = Util->Offset(0x7BDC95C4 - 0x7AF20000);
+DWORD LoggerHookAddress = Util->Offset(0x7B493420 - 0x7AF20000);
+DWORD LoggerReturnAddress = LoggerHookAddress + 5;
+CHAR LoggerHookBackUp[5] = { 0 };
+
+
+__declspec(naked) void HookLoggerCall() {
+    __asm {
+        call LoggerCallAddress;
+        pushad;
+        pushfd;
+
+        push eax;
+        call PrintLogger;
+        add esp, 0x4;
+
+        popfd;
+        popad;
+        jmp LoggerReturnAddress;
+    }
+}
+
 
 void PrintLogger(DWORD eax) {
     if (eax == 0) {
@@ -37,6 +64,16 @@ void PrintLogger(DWORD eax) {
 }
 
 
+void HookLogger() {
+    BYTE JMPCODE[5] = { 0 };
+    JMPCODE[0] = 0xE9;
+    *(DWORD*)&JMPCODE[1] = (DWORD)HookLoggerCall - LoggerHookAddress - 5;
+    ReadProcessMemory(GetCurrentProcess(), (LPVOID)LoggerHookAddress, (LPVOID)LoggerHookBackUp, 5, 0);
+    WriteProcessMemory(GetCurrentProcess(), (LPVOID)LoggerHookAddress, (LPVOID)JMPCODE, 5, 0);
+}
+void UnHookLogger() {
+    WriteProcessMemory(GetCurrentProcess(), (LPVOID)LoggerHookAddress, (LPVOID)LoggerHookBackUp, 5, 0);
+}
 
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID reserved) {
